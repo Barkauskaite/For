e844b6ed2cb1e6506a078659b17a83110a740470
Jolanta Barkauskaite <barkauskaitejolanta@gmail.com>
2021-02-16 14:44:14 +0100

    1

---
+++ For.java
@@ -0,0 +1,7 @@
+public class For{
+    public static void main(String[]args){
+        for(int counter = 1; counter <= 20; counter ++){
+            System.out.println(counter);
+        }
+    }
+}
